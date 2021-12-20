188729da0f7d1048ba7adf83b193b516e08febdf
madamR <33540055+madamR@users.noreply.github.com>
2021-12-20 08:28:24 +0200

    Add files via upload

---
+++ newslettter.php
@@ -0,0 +1,39 @@
+
+
+        
+
+        <div id="newsletter" class="section">
+			<!-- container -->
+			<div class="container">
+				<!-- row -->
+				<div class="row">
+					<div class="col-md-12">
+					
+						<div class="newsletter">
+							<p>Sign Up for the <strong>OFFER UPDATES</strong></p>
+							<form id="offer_form" onsubmit="return false">
+								<input class="input" type="email" id="email" name="email" placeholder="Enter Your Email">
+								<button class="newsletter-btn" value="Sign Up" name="signup_button" type="submit"><i class="fa fa-envelope"></i> Subscribe</button>
+							</form>
+							<div class="" id="offer_msg">
+                                <!--Alert from signup form-->
+                            </div>
+							<ul class="newsletter-follow">
+								<li>
+									<a href="https://web.facebook.com/Adeaz-108423271621996"><i class="fa fa-facebook"></i></a>
+								</li>
+								<li>
+									<a href="https://mobile.twitter.com/ADEaZ9"><i class="fa fa-twitter"></i></a>
+								</li>
+								<li>
+									<a href="https://www.instagram.com/adeaz.ca"><i class="fa fa-instagram"></i></a>
+								</li>
+								
+							</ul>
+						</div>
+					</div>
+				</div>
+				<!-- /row -->
+			</div>
+			<!-- /container -->
+		</div>
